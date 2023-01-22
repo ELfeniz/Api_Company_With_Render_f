2de684a5f5bc159168ff7d5f9eda4e7074c084b0
ELfeniz <juanpavilla1999@gmail.com>
2023-01-22 00:29:54 -0500

    render ready3

--- Backend/settings.py
+++ Backend/settings.py
@@ -65,13 +65,8 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',    # tambien para los cors
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost',
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'https://frontend-apy-company.vercel.app/'
-
-)
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 ROOT_URLCONF = 'Backend.urls'
